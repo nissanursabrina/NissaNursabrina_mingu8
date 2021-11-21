9dd5357d5ceeac48816b0eb5dc88cd563fd39653
nissanursabrina <nissabrina12345@gmail.com>
2021-11-21 20:31:54 +0700

    membuat REST API CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProgramController;
+
 
 
 /*
@@ -31,6 +33,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 Route::get('/profile', function(Request $request) {
 return auth()->user();
 });
+
+Route::resource('programs', ProgramController::class);
 
 // API route for logout user
 Route::post('/logout', [AuthController::class, 'logout']);
